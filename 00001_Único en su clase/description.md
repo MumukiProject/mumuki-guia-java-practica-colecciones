9e84831de9b0b6f8e7c9764b11a20056dfd0c483
agus@mumuki.org <agus@mumuki.org>
2017-07-28 14:15:29 -0400

    Mumuki Export on 2017-07-28 14:15:29 -0400

--- 00001_Único en su clase/description.md
+++ 00001_Único en su clase/description.md
@@ -2,4 +2,4 @@
 
 Lamentablemente en la base donde se almacenan los datos de todas las personas, cometieron un error y duplicaron los datos de ciertas personas. :disappointed_relieved: 
 
-> Implementá un método `ProcesarHuellas` que procese la lista de huellas dactilares a la colección que mejor encaje para eliminar repetidos.
+> Implementá un método `procesarHuellas` que procese la lista de huellas dactilares a la colección que mejor encaje para eliminar repetidos. También nos interesá saber la `cantidadDePersonasRegistradas` en el `RegistroNacional`.
--- 00001_Único en su clase/test.java
+++ 00001_Único en su clase/test.java
@@ -1,6 +1,7 @@
 HuellaDactilar laura = new HuellaDactilar("Laura");
 HuellaDactilar martin = new HuellaDactilar("Martin");
 List<HuellaDactilar> huellas = new ArrayList<>();
+
 @Test
 public void cuandoHay3RepetidosSoloQuedaUno() {
   huellas.add(laura);
@@ -8,4 +9,15 @@ public void cuandoHay3RepetidosSoloQuedaUno() {
   huellas.add(laura);
   RegistroNacional registro = new RegistroNacional(huellas);
   Assert.assertEquals(new HashSet(huellas), registro.procesarHuellas());
+  Assert.assertEquals(1, registro.cantidadDePersonasRegistradas());
+}
+
+@Test
+public void cuandoHay3PeroSolo1Repetido() {
+  huellas.add(laura);
+  huellas.add(laura);
+  huellas.add(martin);
+  RegistroNacional registro = new RegistroNacional(huellas);
+  Assert.assertEquals(new HashSet(huellas), registro.procesarHuellas());
+  Assert.assertEquals(2, registro.cantidadDePersonasRegistradas());
 }
