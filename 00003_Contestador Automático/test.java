82a8a016fc100206a6e736a17116a2d3d1d8ba0e
agus@mumuki.org <agus@mumuki.org>
2017-07-28 15:54:31 -0400

    Mumuki Export on 2017-07-28 15:54:31 -0400

--- 00003_Contestador Automático/test.java
+++ 00003_Contestador Automático/test.java
@@ -1,17 +1,17 @@
 @Test
 public void responde_llama_mas_tarde_si_llama_martin() {
   ContestadorAutomatico ca = new ContestadorAutomatico();
-  Assert.assertEquals("Llamá mas tarde", ca.responderA("martin");
+  Assert.assertEquals("Llamá mas tarde", ca.responderA("martin"));
 }
 
 @Test
 public void responde_con_puntos_suspensivos_si_llama_franco() {
   ContestadorAutomatico ca = new ContestadorAutomatico();
-  Assert.assertEquals("...", ca.responderA("franco");
+  Assert.assertEquals("...", ca.responderA("franco"));
 }
 
 @Test
 public void responde_ahora_no_puedo_si_llama_laura() {
   ContestadorAutomatico ca = new ContestadorAutomatico();
-  Assert.assertEquals("Ahora no puedo, te llamo después", ca.responderA("laura");
+  Assert.assertEquals("Ahora no puedo, te llamo después", ca.responderA("laura"));
 }
