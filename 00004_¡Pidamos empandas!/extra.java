b1d4d4030e521e3f5abc38b57d43c74bcdb7f994
agus@mumuki.org <agus@mumuki.org>
2017-07-28 17:22:43 -0400

    Mumuki Export on 2017-07-28 17:22:43 -0400

--- 00004_¡Pidamos empandas!/extra.java
+++ 00004_¡Pidamos empandas!/extra.java
@@ -1,5 +1,8 @@
 class Empanada {
   String gusto;
+  Empanada(String gusto) {
+    this.gusto = gusto;
+  }
   String getGusto() {
     return this.gusto;
   }
--- 00004_¡Pidamos empandas!/test.java
+++ 00004_¡Pidamos empandas!/test.java
@@ -1,1 +1,14 @@
+Pedido pedido;
+Empanada pollo = new Empanada("Pollo");
+Empanada atun = new Empanada("Atun");
+
+@Before
+public void before() {
+  pedido = new Pedido();
+}
 @Test
+public void 4_de_pollo_2_de_atun() {
+  pedido.setEmpanadas(Arrays.asList(pollo, pollo, atun, pollo, atun, pollo));
+  Assert.assertEquals(4, pedido.realizarPedido().get("Pollo"));
+  Assert.assertEquals(2, pedido.realizarPedido().get("Atun"));
+}
