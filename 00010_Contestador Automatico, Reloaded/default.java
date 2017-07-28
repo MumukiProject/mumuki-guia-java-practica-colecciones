f7cba766ca37f85c5ca065720d68a32119bc23cf
agus@mumuki.org <agus@mumuki.org>
2017-07-28 16:25:25 -0400

    Mumuki Export on 2017-07-28 16:25:25 -0400

--- 00010_Contestador Automatico, Reloaded/default.java
+++ 00010_Contestador Automatico, Reloaded/default.java
@@ -1,1 +1,16 @@
-/*...previousContent...*/
+class ContestadorAutomatico {
+  private HashMap<String, String> respuestas;
+  ContestadorAutomatico() {
+  // Completá el mapa acá   
+  }
+  String responderA(String nombre) {
+    String respuesta = "";
+    if(nombre == "martin")
+      respuesta = "Llamá mas tarde";
+    if(nombre == "laura")
+      respuesta = "Ahora no puedo, te llamo después";
+    if(nombre == "franco")
+      respuesta = "...";
+    return respuesta;
+  }
+}
