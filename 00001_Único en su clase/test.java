c4d0753246a289e57ee175e242c8412226f1256b
agus@mumuki.org <agus@mumuki.org>
2017-07-28 14:02:59 -0400

    Mumuki Export on 2017-07-28 14:02:59 -0400

--- 00001_Único en su clase/test.java
+++ 00001_Único en su clase/test.java
@@ -3,7 +3,7 @@ HuellaDactilar martin = new HuellaDactilar("Martin");
 ArrayList<HuellaDactilar> huellas;
 @Test
 public void cuandoHay3RepetidosSoloQuedaUno() {
-  huellas = Array.asList(laura, laura, laura);
+  huellas = Arrays.asList(laura, laura, laura);
   RegistroNacional registro = new RegistroNacional(huellas);
-  Assert.assertEquals(new Set(huellas), registro.procesarHuellas());
+  Assert.assertEquals(new HashSet(huellas), registro.procesarHuellas());
 }
