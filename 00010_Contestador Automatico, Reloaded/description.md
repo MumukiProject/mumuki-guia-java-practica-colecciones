f97da1e42d26845813d973bacfbb9cf1b2c78c28
agus@mumuki.org <agus@mumuki.org>
2017-07-28 16:24:15 -0400

    Mumuki Export on 2017-07-28 16:24:15 -0400

--- 00010_Contestador Automatico, Reloaded/description.md
+++ 00010_Contestador Automatico, Reloaded/description.md
@@ -1,1 +1,17 @@
-a
+Te presentamos el tipo de coleccion `Map`. Los mapas son estructuras indexadas, que te permiten asignarle a ciertas claves el valor que corresponda.
+
+Por ejemplo, si quisieramos almacenar las edades de algunas personas:
+
+```java
+HashMap<Int, String> edades = new HashMap<>();
+edades.put("Laura", 24);
+edades.put("Martin", 30);
+```
+
+Luego se puede obtener el valor pasandole la clave
+
+``` java
+edades.get("Laura");
+```
+
+> Mejorá utilizando un mapa la solución del ejercicio pasado
--- 00010_Contestador Automatico, Reloaded/test.java
+++ 00010_Contestador Automatico, Reloaded/test.java
@@ -1,1 +1,17 @@
-a
+@Test
+public void responde_llama_mas_tarde_si_llama_martin() {
+  ContestadorAutomatico ca = new ContestadorAutomatico();
+  Assert.assertEquals("Llamá mas tarde", ca.responderA("martin"));
+}
+
+@Test
+public void responde_con_puntos_suspensivos_si_llama_franco() {
+  ContestadorAutomatico ca = new ContestadorAutomatico();
+  Assert.assertEquals("...", ca.responderA("franco"));
+}
+
+@Test
+public void responde_ahora_no_puedo_si_llama_laura() {
+  ContestadorAutomatico ca = new ContestadorAutomatico();
+  Assert.assertEquals("Ahora no puedo, te llamo después", ca.responderA("laura"));
+}
