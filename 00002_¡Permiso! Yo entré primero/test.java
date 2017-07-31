e68e2521ad3b1bd6bde16f4e9537adcce3185edc
agus@mumuki.org <agus@mumuki.org>
2017-07-31 12:28:28 -0400

    Mumuki Export on 2017-07-31 12:28:28 -0400

--- 00002_¡Permiso! Yo entré primero/test.java
+++ 00002_¡Permiso! Yo entré primero/test.java
@@ -1,1 +1,32 @@
+Persona martin = new Persona("Martin");
+Persona laura = new Persona("Laura");
+Persona valeria = new Persona("Valeria");
+Panaderia panaderia;
+
+@Before
+public void before() {
+  panaderia = new Panaderia();
+}
+
 @Test
+public void martin_entra_despues_que_laura() {
+  panaderia.entrarALaPanaderia(laura);
+  panaderia.entrarALaPanaderia(martin);
+  Assert.assertEquals(laura, panaderia.quienSigue());
+}
+
+@Test
+public void solo_entra_valeria() {
+  panaderia.entrarALaPanaderia(valeria);
+  Assert.assertEquals(valeria, panaderia.quienSigue());
+}
+
+@Test
+public void entra_javier_laura_valeria() {
+  panaderia.entrarALaPanaderia(javier);
+  panaderia.entrarALaPanaderia(laura);
+  panaderia.entrarALaPanaderia(valeria);
+  Assert.assertEquals(javier, panaderia.quienSigue());
+  Assert.assertEquals(laura, panaderia.quienSigue());
+  Assert.assertEquals(valeria, panaderia.quienSigue());
+}
