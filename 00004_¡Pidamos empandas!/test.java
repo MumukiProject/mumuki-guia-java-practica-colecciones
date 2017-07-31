b3e2244473139a63d613cc831fb13431254be7da
agus@mumuki.org <agus@mumuki.org>
2017-07-30 20:19:33 -0400

    Mumuki Export on 2017-07-30 20:19:33 -0400

--- 00004_¡Pidamos empandas!/test.java
+++ 00004_¡Pidamos empandas!/test.java
@@ -1,6 +1,7 @@
 Pedido pedido;
 Empanada pollo = new Empanada("Pollo");
 Empanada atun = new Empanada("Atun");
+Empanada jamon = new Empanada("Jamon");
 
 @Before
 public void before() {
@@ -11,4 +12,10 @@ public void cuando_hay_4_de_pollo_2_de_atun() {
   pedido.setEmpanadas(Arrays.asList(pollo, pollo, atun, pollo, atun, pollo));
   Assert.assertEquals(new Integer(4), pedido.realizarPedido().get("Pollo"));
   Assert.assertEquals(new Integer(2), pedido.realizarPedido().get("Atun"));
+}
+@Test
+public void cuando_hay_2_de_jamon_1_de_pollo() {
+  pedido.setEmpanadas(Arrays.asList(pollo, jamon, jamon));
+  Assert.assertEquals(new Integer(2), pedido.realizarPedido().get("Jamon"));
+  Assert.assertEquals(new Integer(1), pedido.realizarPedido().get("Pollo"));
 }
