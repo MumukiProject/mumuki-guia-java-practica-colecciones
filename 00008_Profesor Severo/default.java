b9dbe6bb4b84289ef6507a8a8ad1b3252c53edc6
agus@mumuki.org <agus@mumuki.org>
2017-07-31 16:48:28 -0400

    Mumuki Export on 2017-07-31 16:48:28 -0400

--- 00008_Profesor Severo/default.java
+++ 00008_Profesor Severo/default.java
@@ -1,6 +1,10 @@
 class Quique {
   ProfesorSevero profesor;
   
+  void setProfesor(ProfesorSevero profesor) {
+    this.profesor = profesor;
+  }
+  
   boolean puedeHacerPocion(List<String> pociones) {
     /* Completá tu solución acá */
   }
--- 00008_Profesor Severo/test.java
+++ 00008_Profesor Severo/test.java
@@ -1,1 +1,22 @@
+Map<String, int> ingredientes = new HashMap() {{
+  put("Piel de serpiente arborea", 2);   
+  put("Bezoar", 1); 
+}};
+Quique quique = new Quique();
+Profesor profesor = new Profesor(ingredientes);
+quique.setProfesor(profesor);
+
 @Test
+public void quique_puede_hacer_la_pocion() {
+  Assert.assertEquals(true, quique.puedeHacerPocion(Arrays.asList("Piel de serpiente arborea", "Bezoar")));
+}
+
+@Test
+public void a_severo_le_falta_escarabajo_magico() {
+  Assert.assertEquals(false, quique.puedeHacerPocion(Arrays.asList("Escarabajo Magico", "Bezoar")));
+}
+
+@Test
+public void quique_puede_si_la_pocion_no_implica_ingredientes() {
+  Assert.assertEquals(true, quique.puedeHacerPocion(Arrays.asList()));
+}
