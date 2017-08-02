47e6ff1e2719df4954d15068fd6a46365d62b237
agus@mumuki.org <agus@mumuki.org>
2017-08-02 12:52:01 -0400

    Mumuki Export on 2017-08-02 12:52:01 -0400

--- 00007_¿Está o no está?/test.java
+++ 00007_¿Está o no está?/test.java
@@ -1,6 +1,6 @@
 @Test
 public void primer_primo_es_3() {
   MatematicoHindu ramanujan = new MatematicoHindu(Arrays.asList(2,3,4));
-  Assert.assertEquals(new Optional<Integer>(3), ramanujan.obtenerPrimerPrimo());
-  Assert.assertEquals(new Integer(4), ramanujan.siguienteAlPrimerPrimo());
+  Assert.assertEquals(new Optional.of(3), ramanujan.obtenerPrimerImpar());
+  Assert.assertEquals(new Integer(4), ramanujan.siguienteAlPrimerImpar());
 }
