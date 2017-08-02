e297e8080ea1950618af9fab2b665802fcfaa291
agus@mumuki.org <agus@mumuki.org>
2017-08-02 12:30:46 -0400

    Mumuki Export on 2017-08-02 12:30:46 -0400

--- 00007_¿Está o no está?/extra.java
+++ 00007_¿Está o no está?/extra.java
@@ -2,7 +2,7 @@ import java.lang.Math;
 class TeoriaDeNumeros {
   public static boolean esPrimo(Integer numero) {
       return numero > 2
-        && IntStream.rangeClosed(2, (Integer) Math.sqrt(number))
-        .noneMatch(n -> (number % n == 0));
+        && IntStream.rangeClosed(2, (Integer) Math.sqrt(numero))
+        .noneMatch(n -> (numero % n == 0));
   }  
 }
