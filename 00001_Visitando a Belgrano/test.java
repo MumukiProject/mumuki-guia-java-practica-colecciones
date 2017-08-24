4d6373e7990320dbed3e5b476ae0ec54591dc57d
franco@mumuki.org <franco@mumuki.org>
2017-08-24 01:02:32 -0400

    Mumuki Export on 2017-08-24 01:02:32 -0400

--- 00001_Visitando a Belgrano/test.java
+++ 00001_Visitando a Belgrano/test.java
@@ -13,9 +13,9 @@ public void si_no_registro_sugerencias_entonces_cantidadDnisUnicos_devuelve_0() 
 @Test 
 public void si_registro_3_dnis_diferentes_cantidadDnisUnicos_devuelve_3() {
   LibroDeSugerencias libro = new LibroDeSugerencias();
-  libros.registrarSugerencia("1", "");
-  libros.registrarSugerencia("2", "");
-  libros.registrarSugerencia("3", "");
+  libro.registrarSugerencia("1", "");
+  libro.registrarSugerencia("2", "");
+  libro.registrarSugerencia("3", "");
   Assert.assertEquals(3, libro.cantidadDnisUnicos());
 }
 
@@ -23,17 +23,17 @@ public void si_registro_3_dnis_diferentes_cantidadDnisUnicos_devuelve_3() {
 @Test 
 public void si_registro_3_dnis_guales_cantidadDnisUnicos_devuelve_1() {
   LibroDeSugerencias libro = new LibroDeSugerencias();
-  libros.registrarSugerencia("3", "");
-  libros.registrarSugerencia("3", "");
-  libros.registrarSugerencia("3", "");
+  libro.registrarSugerencia("3", "");
+  libro.registrarSugerencia("3", "");
+  libro.registrarSugerencia("3", "");
   Assert.assertEquals(1, libro.cantidadDnisUnicos());
 }
 
 @Test 
 public void si_registro_3_sugerencias_iguales_sugerencias_devuelve_una_coleccion_con_3_elementos() {
   LibroDeSugerencias libro = new LibroDeSugerencias();
-  libros.registrarSugerencia("3", "Hola");
-  libros.registrarSugerencia("3", "Hola");
-  libros.registrarSugerencia("3", "Hola");
+  libro.registrarSugerencia("3", "Hola");
+  libro.registrarSugerencia("3", "Hola");
+  libro.registrarSugerencia("3", "Hola");
   Assert.assertEquals(3, libro.sugerencias().size());
 }
