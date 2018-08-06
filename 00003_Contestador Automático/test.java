b022fc58a23f599b28f33bf9271b4ef60450c7f8
gus.trucco@gmail.com <gus.trucco@gmail.com>
2018-08-06 14:05:39 -0400

    Mumuki Export on 2018-08-06 14:05:39 -0400

--- 00003_Contestador Automático/test.java
+++ 00003_Contestador Automático/test.java
@@ -1,7 +1,7 @@
 @Test
 public void responde_llama_mas_tarde_si_llama_martin() {
   ContestadorAutomatico ca = new ContestadorAutomatico();
-  Assert.assertEquals("Llamá mas tarde", ca.responderA("martin"));
+  Assert.assertEquals("Llamá más tarde", ca.responderA("martin"));
 }
 
 @Test
