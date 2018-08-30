8e172d4b2f913c6d1483ca74d32d39384fe9865d
gus.trucco@gmail.com <gus.trucco@gmail.com>
2018-08-30 15:28:50 -0400

    Mumuki Export on 2018-08-30 15:28:50 -0400

--- 00011_Cuando tú no estás/test.java
+++ 00011_Cuando tú no estás/test.java
@@ -1,23 +1,23 @@
 @Test
 public void responde_llama_mas_tarde_si_llama_martin() {
   ContestadorAutomatico ca = new ContestadorAutomatico();
-  Assert.assertEquals("Llamá mas tarde", ca.responderA("martin"));
+  Assert.assertEquals("Llamá mas tarde", ca.responderA("Martin"));
 }
 
 @Test
 public void responde_shu_si_llama_lucas() {
   ContestadorAutomatico ca = new ContestadorAutomatico();
-  Assert.assertEquals("¡Shu!", ca.responderA("lucas"));
+  Assert.assertEquals("¡Shu!", ca.responderA("Lucas"));
 }
 
 @Test
 public void responde_ahora_no_puedo_si_llama_laura() {
   ContestadorAutomatico ca = new ContestadorAutomatico();
-  Assert.assertEquals("Ahora no puedo, te llamo después", ca.responderA("laura"));
+  Assert.assertEquals("Ahora no puedo, te llamo después", ca.responderA("Laura"));
 }
 
 @Test
 public void responde_intente_mas_tarde_si_llama_carlos() {
   ContestadorAutomatico ca = new ContestadorAutomatico();
-  Assert.assertEquals("Intente más tarde", ca.responderA("carlos"));
+  Assert.assertEquals("Intente más tarde", ca.responderA("Carlos"));
 }
